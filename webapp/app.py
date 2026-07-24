f8681119445a55c043e78f82696066b3554f7bb0
ArianeDekker <37271177+ArianeDekker@users.noreply.github.com>
2026-07-24 16:14:29 -0500

    description

--- webapp/app.py
+++ webapp/app.py
@@ -62,8 +62,8 @@ DISCIPLINES = ["Sport", "Trad", "Top rope", "Boulder", "Aid", "Mixed", "Ice", "A
 
 st.title("🧗 Climbing Route Finder")
 st.markdown(
-    'Describe your ideal climbing route and get recommendations based on [analyzing](https://arianedekker.github.io/blog)'
-    ' 116k+ Mountain Project climbing routes.'
+    'Describe your ideal climbing route and get recommendations based on [this](https://arianedekker.github.io/blog) analysis'
+    'of 116k+ Mountain Project climbing routes.'
 )
 
 # Loud warning if the real dataset is missing, so the app never quietly
